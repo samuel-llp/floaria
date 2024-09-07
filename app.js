b0759dc17afdf9ee06014c3d10fcade27efdf065
samuel-llp <samuel.llopes01@gmail.com>
2024-09-07 19:59:44 -0300

    enhanced search and clickable cards

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const flowers = [
   {
-    imageUrl: "https://s2-g1.glbimg.com/-0QM0ax64PlQp7wAVOmx8SUrjTQ=/0x0:620x412/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/E/F/2OODNaSUGxe6MJwk5awA/ipe-amarelo-ananda-porto.jpg",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Handroanthus_serratifolius.jpg",
     name: "Ipê-amarelo",
     scientificName: "Handroanthus albus",
     description: "O Ipê-amarelo é uma árvore icônica do Brasil, conhecida por suas vibrantes flores amarelas que florescem durante o inverno. Ela é famosa por criar um espetáculo dourado nas paisagens, especialmente quando suas flores caem, formando um tapete dourado no solo.",
     origin: "Regiões tropicais do Brasil.",
     floweringPeriod: "Inverno.",
     cultivation: "Cultivado em áreas com bastante sol e solo bem drenado.",
+    tag: "flor árvore inverno amarelo brasil tropical",
     infoLink: "https://pt.wikipedia.org/wiki/Ip%C3%AA-amarelo-flor-de-algod%C3%A3o",
   },
   {
@@ -17,6 +18,7 @@ const flowers = [
     origin: "Ásia e adaptado ao Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Precisa de sol pleno e rega frequente para crescer saudavelmente.",
+    tag: "flor arbusto primavera verão paisagismo chá",
     infoLink: "https://pt.wikipedia.org/wiki/Hibiscus",
   },
   {
@@ -27,16 +29,18 @@ const flowers = [
     origin: "Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Requer clima quente e úmido, com boa drenagem e luz indireta.",
+    tag: "orquídea flor tropical primavera verão decoração",
     infoLink: "https://pt.wikipedia.org/wiki/Cattleya",
   },
   {
-    imageUrl: "https://minhasplantas3.s3.amazonaws.com/media/duvidas/perguntas/Adubo-Bromelia.jpg",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/BROMELIA.JPG/800px-BROMELIA.JPG",
     name: "Bromélia",
     scientificName: "Bromeliaceae",
     description: "Com suas flores exóticas e folhas espinhosas, a bromélia é uma planta adaptada a climas tropicais, sendo uma das favoritas para compor jardins tropicais. Sua capacidade de armazenar água entre suas folhas a torna única.",
     origin: "Brasil.",
     floweringPeriod: "Variável.",
     cultivation: "Prefere solos bem drenados e necessita de rega moderada.",
+    tag: "flor tropical exótica jardim bromélia água",
     infoLink: "https://pt.wikipedia.org/wiki/Brom%C3%A9lia",
   },
   {
@@ -47,6 +51,7 @@ const flowers = [
     origin: "Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Necessita de clima tropical e solo bem drenado.",
+    tag: "flor árvore símbolo brasil madeira tropical",
     infoLink: "https://pt.wikipedia.org/wiki/Pau-brasil",
   },
   {
@@ -57,6 +62,7 @@ const flowers = [
     origin: "Brasil.",
     floweringPeriod: "Verão.",
     cultivation: "Prefere clima seco e solo bem drenado.",
+    tag: "flor resistência seca decoração arranjos",
     infoLink: "https://pt.wikipedia.org/wiki/Sempre-viva",
   },
   {
@@ -67,6 +73,7 @@ const flowers = [
     origin: "Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Sol pleno e solo bem drenado.",
+    tag: "flor árvore madeira culinária medicinal",
     infoLink: "https://pt.m.wikipedia.org/wiki/Ficheiro:Flor-do-jatoba.jpg",
   },
   {
@@ -77,6 +84,7 @@ const flowers = [
     origin: "Amazônia, Brasil.",
     floweringPeriod: "Verão.",
     cultivation: "Requer águas rasas e tranquilas, com clima quente e úmido.",
+    tag: "flor aquática amazônia noite verão",
     infoLink: "https://pt.wikipedia.org/wiki/Victoria_amazonica",
   },
   {
@@ -87,6 +95,7 @@ const flowers = [
     origin: "Nordeste, Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Precisa de sol pleno e rega mínima, adaptado a solos secos.",
+    tag: "flor cacto sertão nordeste semiárido paisagismo",
     infoLink: "https://pt.wikipedia.org/wiki/Cereus_jamacaru",
   },
   {
@@ -97,18 +106,20 @@ const flowers = [
     origin: "Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Precisa de sol pleno e solo fértil e bem drenado.",
+    tag: "flor exótica suco maracujá alimentação",
     infoLink: "https://pt.wikipedia.org/wiki/Passiflora_edulis",
   },
   {
-    imageUrl: "https://inpn.mnhn.fr/photos/uploads/webtofs/inpn/4/276414.jpg",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Clusia_fluminensis.jpg/1280px-Clusia_fluminensis.jpg",
     name: "Clúsia",
     scientificName: "Clusia fluminensis",
     description: "A Clúsia é uma planta tropical com flores brancas e folhas espessas. Ela é muito resistente e é usada frequentemente no paisagismo para formar cercas vivas ou preencher grandes espaços devido à sua folhagem densa.",
     origin: "Mata Atlântica, Brasil.",
-    floweringPeriod: "Primavera.",
-    cultivation: "Sol pleno ou meia sombra, solo bem drenado.",
+    floweringPeriod: "Verão.",
+    cultivation: "Prefere sol pleno e solos bem drenados.",
+    tag: "flor paisagismo cerca viva tropical resistência",
     infoLink: "https://pt.wikipedia.org/wiki/Clusia",
-  },
+  },
   {
     imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Paepalanthus_eriophaeus.jpg/1280px-Paepalanthus_eriophaeus.jpg",
     name: "Paepalanthus",
@@ -117,6 +128,7 @@ const flowers = [
     origin: "Campos rupestres, Brasil.",
     floweringPeriod: "Ano todo, dependendo da espécie.",
     cultivation: "Sol pleno, solos arenosos e bem drenados.",
+    tag: "flor rupestre esférica delicada rochoso árido",
     infoLink: "https://pt.wikipedia.org/wiki/Paepalanthus",
   },
   {
@@ -127,6 +139,7 @@ const flowers = [
     origin: "América Tropical.",
     floweringPeriod: "Ano todo.",
     cultivation: "Sol pleno, solos pobres e bem drenados.",
+    tag: "arbusto resistente flores coloridas paisagismo",
     infoLink: "https://pt.wikipedia.org/wiki/Cambar%C3%A1_(%C3%A1rvore)",
   },
   {
@@ -137,6 +150,7 @@ const flowers = [
     origin: "Florestas tropicais.",
     floweringPeriod: "Ano todo em condições ideais.",
     cultivation: "Sol pleno ou meia-sombra, solos ricos e úmidos.",
+    tag: "planta exótica vibrante beija-flores tropical",
     infoLink: "https://pt.wikipedia.org/wiki/Helic%C3%B4nia",
   },
   {
@@ -147,6 +161,7 @@ const flowers = [
     origin: "Mata Atlântica, Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Sol pleno, solos ricos e bem drenados.",
+    tag: "arbusto ornamental flores mudam cor mata atlântica",
     infoLink: "https://pt.wikipedia.org/wiki/Tibouchina_mutabilis",
   },
   {
@@ -157,6 +172,7 @@ const flowers = [
     origin: "Brasil e Argentina.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Sol pleno, solos ricos e úmidos.",
+    tag: "flor vermelha brilhante aves beleza brasileira",
     infoLink: "https://pt.wikipedia.org/wiki/Corticeira",
   },
   {
@@ -167,6 +183,7 @@ const flowers = [
     origin: "América Central, popular no Brasil.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Sol pleno, solos bem drenados e pouca rega.",
+    tag: "flor perfumada tropical paisagismo beleza",
     infoLink: "https://pt.wikipedia.org/wiki/Jasmim-manga",
   },
   {
@@ -177,6 +194,7 @@ const flowers = [
     origin: "América do Sul.",
     floweringPeriod: "Primavera a outono.",
     cultivation: "Sol pleno, solos bem drenados e úmidos.",
+    tag: "trepadeira flores azuis roxas cercas pergolados",
     infoLink: "https://pt.wikipedia.org/wiki/Gl%C3%B3ria-da-manh%C3%A3",
   },
   {
@@ -187,6 +205,7 @@ const flowers = [
     origin: "América Central e do Sul.",
     floweringPeriod: "Verão.",
     cultivation: "Sol pleno ou meia-sombra, solos úmidos.",
+    tag: "flor árvore fortuna flores grandes prosperidade",
     infoLink: "https://pt.wikipedia.org/wiki/Pachira_aquatica",
   },
   {
@@ -197,6 +216,7 @@ const flowers = [
     origin: "América Tropical.",
     floweringPeriod: "Ano todo.",
     cultivation: "Meia-sombra, solos ricos e úmidos.",
+    tag: "flor interna paz pureza brácteas brancas",
     infoLink: "https://pt.wikipedia.org/wiki/Spathiphyllum_wallisii",
   },
   {
@@ -207,9 +227,17 @@ const flowers = [
     origin: "Nativa das florestas tropicais da Ásia.",
     floweringPeriod: "Primavera e verão.",
     cultivation: "Meia-sombra, solos bem drenados e úmidos.",
+    tag: "orquídea exótica sapatos elegantes cores variadas",
     infoLink: "https://pt.wikipedia.org/wiki/Paphiopedilum",
-  }
+  },
 ];
+
+function formatString(str) {
+  return str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase();
+}
 
 function formatString(str) {
   return str
@@ -223,7 +251,8 @@ function search() {
   
   const filteredFlowers = flowers.filter(flower => 
     formatString(flower.name).includes(query) ||
-    formatString(flower.scientificName).includes(query)
+    formatString(flower.scientificName).includes(query) ||
+    formatString(flower.tag).includes(query)
   );
 
   updateFlower(filteredFlowers);
@@ -231,30 +260,54 @@ function search() {
 
 function renderFlower(flower) {
   return `
-    <div class="card-hidden">
-      <div class="image-container">
-        <img src="${flower.imageUrl}" alt="${flower.name}" class="image glass-card"/>
+    <div class="flower-card">
+      <div class="card-hidden">
+        <div class="image-container">
+          <img src="${flower.imageUrl}" alt="${flower.name}" class="image glass-card"/>
+        </div>
+        <div class="labels">
+          <div class="names">
+            <h2 class="font-xl">${flower.name}</h2>
+            <p class="font-caption-w">${flower.scientificName}</p>
+          </div>
+          <p class="font-caption">${flower.description}</p>
+        </div>
       </div>
-      <div class="labels">
-        <div class="names">
-          <h2 class="font-xl">${flower.name}</h2>
-          <p class="font-caption-w">${flower.scientificName}</p>
+      
+      <div class="card-preview glass-card">
+        <div class="image-container">
+          <img src="${flower.imageUrl}" alt="${flower.name}" class="image glass-card"/>
         </div>
-        <p class="font-caption">${flower.description}</p>
-        <div class="informations">
-          <p class="font-caption"><span class="font-caption-w">Origem:</span> ${flower.origin}</p>
-          <p class="font-caption"><span class="font-caption-w">Floração:</span> ${flower.floweringPeriod}</p>
-          <p class="font-caption"><span class="font-caption-w">Cultivo:</span> ${flower.cultivation}</p>
+        <div class="labels">
+          <div class="names">
+            <h2 class="font-xl">${flower.name}</h2>
+            <p class="font-caption-w">${flower.scientificName}</p>
+          </div>
+          <p class="font-caption">${flower.description}</p>
+          <div class="information">
+            <p class="font-caption"><span class="font-caption-w">Origem:</span> ${flower.origin}</p>
+            <p class="font-caption"><span class="font-caption-w">Floração:</span> ${flower.floweringPeriod}</p>
+            <p class="font-caption"><span class="font-caption-w">Cultivo:</span> ${flower.cultivation}</p>
+          </div>
+          <a href="${flower.infoLink}" target="_blank" class="button-wiki glass-secundary">Saiba mais</a>
         </div>
-        <a href="${flower.infoLink}" target="_blank" class="button-wiki glass-secundary">Saiba mais</a>
       </div>
     </div>
+    <div class="overlay"></div>
   `;
 }
 
 function updateFlower(flowers) {
   const section = document.getElementById("results");
-  section.innerHTML = flowers.map(renderFlower).join('');
+  
+  const sortedFlowers = flowers.sort((a, b) => a.name.localeCompare(b.name));
+
+  if (sortedFlowers.length === 0) {
+    section.innerHTML = `<p class="font-caption">Nada foi encontrado. Tente outra busca.</p>`;
+  } else {
+    section.innerHTML = sortedFlowers.map(renderFlower).join('');
+    attachCardToggle();
+  }
 }
 
 function handleSearchOnEnter(event) {
@@ -265,5 +318,31 @@ function handleSearchOnEnter(event) {
 }
 
 document.getElementById('searchInput').addEventListener('keydown', handleSearchOnEnter);
+
+function attachCardToggle() {
+  document.querySelectorAll('.flower-card').forEach(card => {
+    card.addEventListener('click', (event) => {
+      event.stopPropagation();
+      
+      const previewCard = card.querySelector('.card-preview');
+      const overlay = document.querySelector('.overlay');
+      
+      if (previewCard.style.display === 'block') {
+        previewCard.style.display = 'none';
+        overlay.style.display = 'none';
+      } else {
+        previewCard.style.display = 'block';
+        overlay.style.display = 'block';
+      }
+    });
+  });
+  
+  document.querySelector('.overlay').addEventListener('click', () => {
+    document.querySelectorAll('.card-preview').forEach(previewCard => {
+      previewCard.style.display = 'none';
+    });
+    document.querySelector('.overlay').style.display = 'none';
+  });
+}
 
 updateFlower(flowers);
